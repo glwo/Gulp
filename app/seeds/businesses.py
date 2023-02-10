6bb60295684181ba52213ccefdc1b6ba3ea26efa
nednguyen <rpmshockwave@gmail.com>
2023-02-10 14:16:29 -0500

    business and business_image model

---
+++ app/seeds/businesses.py
@@ -0,0 +1,19 @@
+from app.models import db, Business, environment, SCHEMA
+
+# Add demo businesses seed
+def seed_businesses():
+  brandons_donuts = Business(
+    id
+    store_name
+    description
+    city
+    state
+    address
+    zipcode
+    business_type
+    opening_time
+    closing_time
+    phone_num
+    avg_rating
+    num_reviews
+  )
