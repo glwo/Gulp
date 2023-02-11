118eeabd45de6e1aa0d603a8670b4680d207e14b
glen <glenwojnar@yahoo.com>
2023-02-11 08:55:45 -0500

    resolved merge conflicts

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,5 @@
 from .db import db
 from .user import User
-<<<<<<< HEAD
 from .filter import Filter
-=======
 from .review import Review
->>>>>>> dad3949 (fixed branch)
 from .db import environment, SCHEMA
