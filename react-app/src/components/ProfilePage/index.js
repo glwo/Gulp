8bbda1284b024c73ef0adc42e6e7718c5b597991
glen <glenwojnar@yahoo.com>
2023-02-12 16:53:13 -0500

    fixed syntax issue

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { updateProfile} from '../../store/profile'
 
 export default function ProfilePage() {
     const sessionUser = useSelector(state => state.session.user);
@@ -17,8 +18,9 @@ export default function ProfilePage() {
                         <img id='profilepic' src={sessionUser.img_url} alt="Profile picture could not be found"></img>
                     </li>
                     <li>User: {sessionUser.username}</li>
-                    <li>Name: {sessionUser.first_name} {user.last_name}</li>
+                    <li>Name: {sessionUser.first_name} {sessionUser.last_name}</li>
                     <li>Email: {sessionUser.email}</li>
+                    <button>Update Profile</button>
                     </>
                     }
                 </div>
