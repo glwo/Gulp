fabaa147dbc35c6387c6314ed86987c1c6c27636
glen <glenwojnar@yahoo.com>
2023-02-16 14:15:49 -0500

    added business seeds

--- app/seeds/business_image.py
+++ app/seeds/business_image.py
@@ -108,6 +108,186 @@ def seed_business_images():
     preview=True,
     business_id=12
   )
+  anchor_image1 = BusinessImage(
+    image_url="http://www.sfheritage.org/wp-content/uploads/2020/06/June_23_2020_Kerri-scaled.jpg",
+    preview=True,
+    business_id=13
+  )
+  anchor_image2 = BusinessImage(
+    image_url="https://s3-media0.fl.yelpcdn.com/bphoto/MiVozTDhE1rpUZ8PghXmDg/348s.jpg",
+    preview=True,
+    business_id=13
+  )
+  deliboard_image1 = BusinessImage(
+    image_url="https://static.wixstatic.com/media/15e88d_7996e9b9ace643aa87f5f18265ced734~mv2.jpg/v1/fill/w_2500,h_1779,al_c/15e88d_7996e9b9ace643aa87f5f18265ced734~mv2.jpg",
+    preview=True,
+    business_id=14
+  )
+  deliboard_image2 = BusinessImage(
+    image_url="https://img.hoodline.com/uploads/story/image/11237/DB_3_cmp.jpg",
+    preview=True,
+    business_id=14
+  )
+  brendas_image1 = BusinessImage(
+    image_url="https://infatuation.imgix.net/media/reviews/brendas-french-soul-food/banners/Brendas_0.jpg",
+    preview=True,
+    business_id=15
+  )
+  brendas_image2 = BusinessImage(
+    image_url="https://frenchsoulfood.com/wp-content/uploads/2020/07/BFSF_Site-Headers_2020_bennies.jpg",
+    preview=True,
+    business_id=15
+  )
+  boulevard_image1 = BusinessImage(
+    image_url="https://resizer.otstatic.com/v2/photos/xlarge/4/31869098.jpg",
+    preview=True,
+    business_id=16
+  )
+  boulevard_image2 = BusinessImage(
+    image_url="https://cdn.vox-cdn.com/thumbor/-CrjcuYOITDXAmnvQb3P8-32DiE=/0x0:2000x1333/1200x0/filters:focal(0x0:2000x1333):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22888588/Boulevard_PChang_2796.jpg",
+    preview=True,
+    business_id=16
+  )
+  toyota_image1 = BusinessImage(
+    image_url="https://fastly.4sqi.net/img/general/200x200/1106017_xj1so0iaScaGiTp0o3RNUqaZUMjvrQCtmasH-eMLFVg.jpg",
+    preview=True,
+    business_id=17
+  )
+  toyota_image2 = BusinessImage(
+    image_url="https://static.fzinternal.com/dealers/5ee8e2d22ccde.jpg",
+    preview=True,
+    business_id=17
+  )
+  JT_image1 = BusinessImage(
+    image_url="https://s3-media0.fl.yelpcdn.com/bphoto/DvtmGFrvp82xviVL3D3sdQ/348s.jpg",
+    preview=True,
+    business_id=18
+  )
+  JT_image2 = BusinessImage(
+    image_url="https://s3-media0.fl.yelpcdn.com/bphoto/byDgqoLfWDhDVaS8bMCG3g/348s.jpg",
+    preview=True,
+    business_id=18
+  )
+  oreilly_image1 = BusinessImage(
+    image_url="https://securecdn.pymnts.com/wp-content/uploads/2022/07/OReilly-Automotive-1000x600.jpg",
+    preview=True,
+    business_id=19
+  )
+  oreilly_image2 = BusinessImage(
+    image_url="https://fastly.4sqi.net/img/general/600x600/487386563_umq5F1WkSfnveF0f9ak_kA0HjdvXjxRU97Vdgnb_3u0.jpg",
+    preview=True,
+    business_id=19
+  )
+  abcmotor_image1 = BusinessImage(
+    image_url="https://www.abcmotorcredit.com/images/template/logo.png",
+    preview=True,
+    business_id=20
+  )
+  abcmotor_image2 = BusinessImage(
+    image_url="https://abcmotorcredit.com/images/content/we-buy-cars-copy-jpshwi.jpg",
+    preview=True,
+    business_id=20
+  )
+  riteway_image1 = BusinessImage(
+    image_url="https://www.ritewayautos.com/graphics/logo.png",
+    preview=True,
+    business_id=21
+  )
+  riteway_image2 = BusinessImage(
+    image_url="https://www.rite-wayelectric.com/wp-content/uploads/2019/02/Rite-Way-Electric-Building-300x283.jpg",
+    preview=True,
+    business_id=21
+  )
+  code_image1 = BusinessImage(
+    image_url="https://d2zdpiztbgorvt.cloudfront.net/us/images/180036/cover_155601529000.jpeg",
+    preview=True,
+    business_id=22
+  )
+  code_image2 = BusinessImage(
+    image_url="http://1.bp.blogspot.com/-FoOaTPnyG2A/T2o_908hfJI/AAAAAAAAB5Q/9tGgb2CoHmY/s1600/Screen+shot+2012-03-21+at+3.53.11+PM.png",
+    preview=True,
+    business_id=22
+  )
+  bottega_image1 = BusinessImage(
+    image_url="https://d2zdpiztbgorvt.cloudfront.net/us/images/180036/cover_155601529000.jpeg",
+    preview=True,
+    business_id=23
+  )
+  bottega_image2 = BusinessImage(
+    image_url="http://1.bp.blogspot.com/-FoOaTPnyG2A/T2o_908hfJI/AAAAAAAAB5Q/9tGgb2CoHmY/s1600/Screen+shot+2012-03-21+at+3.53.11+PM.png",
+    preview=True,
+    business_id=23
+  )
+  jims_image1 = BusinessImage(
+    image_url="https://media-cdn.tripadvisor.com/media/photo-s/12/32/90/fb/photo3jpg.jpg",
+    preview=True,
+    business_id=24
+  )
+  jims_image2 = BusinessImage(
+    image_url="https://media-cdn.tripadvisor.com/media/photo-s/0e/b0/ff/e5/i-love-a-restaurant-that.jpg",
+    preview=True,
+    business_id=24
+  )
+  homeinstead_image1 = BusinessImage(
+    image_url="https://d13iq96prksfh0.cloudfront.net/cdn/photos/125277/730x450%23.png",
+    preview=True,
+    business_id=25
+  )
+  homeinstead_image2 = BusinessImage(
+    image_url="https://d13iq96prksfh0.cloudfront.net/cdn/photos/125303/730x450%23.jpeg",
+    preview=True,
+    business_id=25
+  )
+  castros_image1 = BusinessImage(
+    image_url="https://castroscleaningserviceca.com/src/images/elements/slider/mobile/1.jpg",
+    preview=True,
+    business_id=26
+  )
+  castros_image2 = BusinessImage(
+    image_url="https://s3-media0.fl.yelpcdn.com/bphoto/bflPFkKWY31HN2A61o_JOA/348s.jpg",
+    preview=True,
+    business_id=26
+  )
+  grubstake_image1 = BusinessImage(
+    image_url="https://cdn.vox-cdn.com/thumbor/_SX5r3PKm-umiFQMNjUi0Lx5K7g=/0x0:700x525/1200x800/filters:focal(294x207:406x319)/cdn.vox-cdn.com/uploads/chorus_image/image/65970522/grubstake.0.0.jpg",
+    preview=True,
+    business_id=27
+  )
+  grubstake_image2 = BusinessImage(
+    image_url="https://miro.medium.com/max/1000/1*v7R7Za2rWu2h0z64N3XHSA.jpeg",
+    preview=True,
+    business_id=27
+  )
+  sonandgarden_image1 = BusinessImage(
+    image_url="https://images.otstatic.com/prod1/47151359/2/large.png",
+    preview=True,
+    business_id=28
+  )
+  sonandgarden_image2 = BusinessImage(
+    image_url="https://cdn.vox-cdn.com/thumbor/yzS7QUonYkp8EUz0ocR-z1L9qDs=/0x0:1046x1046/1200x0/filters:focal(0x0:1046x1046):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/21869393/89029264_127355888831075_6234785980998483968_o.jpg",
+    preview=True,
+    business_id=28
+  )
+  artys_image1 = BusinessImage(
+    image_url="https://s3-media0.fl.yelpcdn.com/bphoto/bGAaUfAYoD8dXd9OMQZ4zA/348s.jpg",
+    preview=True,
+    business_id=29
+  )
+  artys_image2 = BusinessImage(
+    image_url="https://fastly.4sqi.net/img/general/200x200/uA3bwsSQf-QznOpg9Mpf-JMunscPg8sdUi7kCe1632I.jpg",
+    preview=True,
+    business_id=29
+  )
+  mjm_image1 = BusinessImage(
+    image_url="https://www.mjmjanitor.com/wp-content/uploads/2018/05/MJM_logo.png",
+    preview=True,
+    business_id=30
+  )
+  mjm_image2 = BusinessImage(
+    image_url="https://www.mjmjanitor.com/wp-content/uploads/2018/05/mjmmaintenance_518586238.jpg",
+    preview=True,
+    business_id=30
+  )
 
   db.session.add(brandons_donuts_image)
   db.session.add(da_andrea_image)
@@ -130,6 +310,43 @@ def seed_business_images():
   db.session.add(moe_image2)
   db.session.add(enzi_image1)
   db.session.add(enzi_image2)
+  db.session.add(anchor_image1)
+  db.session.add(anchor_image2)
+  db.session.add(deliboard_image1)
+  db.session.add(deliboard_image2)
+  db.session.add(brendas_image1)
+  db.session.add(brendas_image2)
+  db.session.add(boulevard_image1)
+  db.session.add(boulevard_image2)
+  db.session.add(toyota_image1)
+  db.session.add(toyota_image2)
+  db.session.add(JT_image1)
+  db.session.add(JT_image2)
+  db.session.add(oreilly_image1)
+  db.session.add(oreilly_image2)
+  db.session.add(abcmotor_image1)
+  db.session.add(abcmotor_image2)
+  db.session.add(riteway_image1)
+  db.session.add(riteway_image2)
+  db.session.add(code_image1)
+  db.session.add(code_image2)
+  db.session.add(bottega_image1)
+  db.session.add(bottega_image2)
+  db.session.add(jims_image1)
+  db.session.add(jims_image2)
+  db.session.add(homeinstead_image1)
+  db.session.add(homeinstead_image2)
+  db.session.add(castros_image1)
+  db.session.add(castros_image2)
+  db.session.add(grubstake_image1)
+  db.session.add(grubstake_image2)
+  db.session.add(sonandgarden_image1)
+  db.session.add(sonandgarden_image2)
+  db.session.add(artys_image1)
+  db.session.add(artys_image2)
+  db.session.add(mjm_image1)
+  db.session.add(mjm_image2)
+
   db.session.commit()
 
 def undo_business_images():
@@ -137,5 +354,5 @@ def undo_business_images():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM business_images")
-        
+
     db.session.commit()
--- react-app/src/components/BusinessCard/index.js
+++ react-app/src/components/BusinessCard/index.js
@@ -11,7 +11,7 @@ const BusinessCard = ({ business }) => {
   return (
     <div className="businessCard-main-div">
       <Link to={`/business/${business.id}`}>
-        <img className="business-preview-image" src={business.business_images[0].image_url} />
+        <img className="business-preview-image" src={business.business_images[0] !== undefined ? business.business_images[0].image_url : null} alt="No Business Image found" />
         <div>
           <div className="businessStoreName">{business.store_name}</div>
           <div><i class="fa-solid fa-star"></i> {business.avg_rating} - <i class="fa-solid fa-user-check"></i> {business.num_reviews} Reviews</div>
