ff73be7e6308dcb3dacf64583c95d4e113086c2a
nednguyen <rpmshockwave@gmail.com>
2023-03-21 17:51:04 -0400

    google api business detail

--- react-app/src/components/BusinessDetail/index.js
+++ react-app/src/components/BusinessDetail/index.js
@@ -1,5 +1,5 @@
 //react-app/src/components/BusinessDetail/index.js
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { thunkLoadAllBusinesses } from "../../store/business";
 import { Link, useParams } from "react-router-dom";
@@ -8,15 +8,28 @@ import CreateReviewForm from "../Reviews/CreateReviewForm";
 import CreateReviewModal from "../Reviews/EditReviewModal";
 import OpenModalButton from "../OpenModalButton";
 import ReviewDetails from "../Reviews/ReviewDetails";
+import { getKey } from "../../store/map";
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { MarkerClusterer } from "@react-google-maps/api";
+import { getGeocode, getLatLng } from 'use-places-autocomplete'
+import Geocode from 'react-geocode'
+import Map from "../Map";
+import { Cluster } from "@googlemaps/markerclusterer";
 
 const BusinessDetail = () => {
   const [openModal, setOpenModal] = useState(false);
   const { businessId } = useParams();
   const dispatch = useDispatch();
   const business = useSelector(state => state.business.businesses)
+  const [location, setLocation] = useState()
+  const [isLoading, setLoading] = useState(true)
+  const key = useSelector(state => state.key)
+  const realKey = key?.key
   const currentBusiness = Object.values(business).find(business => business.id == businessId)
   const suggestBusiness = Object.values(business).filter(business => business.city == currentBusiness.city && business.business_type == currentBusiness.business_type && business != currentBusiness)
-  
+  if (key) {
+    Geocode.setApiKey(realKey)
+  }
   let businessReviews;
   if (currentBusiness) {
     businessReviews = currentBusiness.review
@@ -24,25 +37,80 @@ const BusinessDetail = () => {
 
   useEffect(() => {
     dispatch(thunkLoadAllBusinesses())
+    dispatch(getKey())
   }, [dispatch, businessId])
 
   const reviewFilter = (array, num) => {
     if (array) {
       const result = array.filter(review => review.rating == num)
       return result.length
-    } 
+    }
     return null
-  } 
+  }
+
+  useEffect(() => {
+    if (currentBusiness && key) {
+      const realAddress = currentBusiness.address + " " + currentBusiness.city + " " + currentBusiness.state
+      Geocode.fromAddress(realAddress).then(
+        (response) => {
+          const { lat, lng } = response.results[0].geometry.location;
+          setLocation({lat, lng})
+          setLoading(false)
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }, [currentBusiness])
+
+  const { isLoaded } = useLoadScript({
+    id: 'google-map-script',
+    googleMapsApiKey: realKey,
+  });
+  const containerStyle = {
+    width: '300px',
+    height: '250px'
+  };
+
+  const CurrentMap = () => {
+
+    return isLoaded && (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={{ lng: location.lng, lat: location.lat }}
+        zoom={9}
+      >
+        <MarkerClusterer>
+          {(cluster) => {
+            return (
+              < Marker
+                key={currentBusiness.id}
+                position={{ lat: location.lat, lng: location.lng }}
+              />
+            )
+          }}
+        </MarkerClusterer>
+      </GoogleMap>
+    )
+  }
 
   if (!currentBusiness) {
     return null
   }
 
+  if (isLoading == true) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
 
   return (
     <>
       <div className="business-image-div">
-        {currentBusiness && currentBusiness.business_images.map(image => <img className="business-image" src={image.image_url}/>)}
+        {currentBusiness && currentBusiness.business_images.map(image => <img className="business-image" src={image.image_url} />)}
       </div>
       <div className="main-div">
         <div className="title-description-main-div">
@@ -68,7 +136,13 @@ const BusinessDetail = () => {
             <h3>Location & Hours</h3>
             <div className="location-hour-main-div">
               <div>
-                <div>Google map here</div>
+                <div className="businessMap">
+                  {/* {isLoading 
+                  ? <div>Loading...</div> 
+                  : currentBusiness && key && <Map currentBusiness={currentBusiness} location={location}/>} */}
+                  {/* {lat && lng && <Map currentBusiness={currentBusiness} lat={lat} lng={lng}/>} */}
+                  <CurrentMap />
+                </div>
                 <ul className="nl">
                   <li>{currentBusiness.address}</li>
                   <li>{currentBusiness.city}, {currentBusiness.state} {currentBusiness.zipcode}</li>
@@ -146,7 +220,7 @@ const BusinessDetail = () => {
               </div>
               <div className="all-review-div">
                 <div>
-                < ReviewDetails businessId={currentBusiness.id} />
+                  < ReviewDetails businessId={currentBusiness.id} />
                 </div>
               </div>
             </div>
@@ -175,28 +249,28 @@ const BusinessDetail = () => {
               <h3>You might also consider</h3>
               <div className="suggest-business-main-div">
                 {suggestBusiness[0] &&
-                <Link className="suggest-business-single-div" to={`/business/${suggestBusiness[0].id}`}>
-                  <img className="suggest-business-image" src={suggestBusiness[0].business_images[0].image_url}/>
-                  <div className="suggest-business-div">
-                    <div>
-                      {suggestBusiness[0].store_name}
+                  <Link className="suggest-business-single-div" to={`/business/${suggestBusiness[0].id}`}>
+                    <img className="suggest-business-image" src={suggestBusiness[0].business_images[0].image_url} />
+                    <div className="suggest-business-div">
+                      <div>
+                        {suggestBusiness[0].store_name}
+                      </div>
+                      <div>
+                        {suggestBusiness[0].avg_rating >= 1 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
+                        {suggestBusiness[0].avg_rating >= 2 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
+                        {suggestBusiness[0].avg_rating >= 3 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
+                        {suggestBusiness[0].avg_rating >= 4 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
+                        {suggestBusiness[0].avg_rating >= 5 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
+                      </div>
+                      <div>
+                        {suggestBusiness[0].num_reviews} reviews
+                      </div>
                     </div>
-                    <div>
-                      {suggestBusiness[0].avg_rating >= 1 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
-                      {suggestBusiness[0].avg_rating >= 2 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
-                      {suggestBusiness[0].avg_rating >= 3 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
-                      {suggestBusiness[0].avg_rating >= 4 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
-                      {suggestBusiness[0].avg_rating >= 5 ? <i className="fas fa-solid fa-star red"></i> : <i className="fas fa-solid fa-star gray"></i>}
-                    </div>
-                    <div>
-                      {suggestBusiness[0].num_reviews} reviews
-                    </div>
-                  </div>
-                </Link>
+                  </Link>
                 }
                 {suggestBusiness[1] &&
                   <Link className="suggest-business-single-div" to={`/business/${suggestBusiness[1].id}`}>
-                    <img className="suggest-business-image" src={suggestBusiness[1].business_images[0].image_url}/>
+                    <img className="suggest-business-image" src={suggestBusiness[1].business_images[0].image_url} />
                     <div className="suggest-business-div">
                       <div>
                         {suggestBusiness[1].store_name}
---
+++ react-app/src/components/Map/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { GoogleMap, useLoadScript, Marker, MarkerClusterer } from '@react-google-maps/api';
+import { getGeocode, getLatLng } from 'use-places-autocomplete'
+import { getKey } from "../../store/map";
+
+const Map = ({currentBusiness}, {lat}, lng) => {
+  console.log(lng)
+  const [location, setLocation] = useState('')
+  const dispatch = useDispatch();
+  const key = useSelector(state => state.key)
+  const realKey = key?.key
+
+  // useEffect(async () => {
+  //   const res = await getGeocode({ address: currentBusiness.address + currentBusiness.city })
+  //   const {lat, lng} = getLatLng(res[0])
+  //   setLocation({lat, lng})
+  //   console.log({lat, lng})
+  // }, [])
+  // let location;
+
+  // if (currentBusiness) {
+
+  //   const getLocation = async () => {
+  //     const res = getGeocode({ address: currentBusiness.address + currentBusiness.city })
+  //       .then(async (res) => {
+  //         const { lat, lng } = await getLatLng(res[0])
+  //         location = { lat, lng }
+  //       })
+  //     return location
+  //   }
+  //   setTimeout(() => {
+  //     console.log(location)
+  //   }, 2000)
+  // }
+  const { isLoaded } = useLoadScript({
+    id: 'google-map-script',
+    googleMapsApiKey: realKey,
+  });
+  const containerStyle = {
+    width: '300px',
+    height: '250px'
+  };
+  // if (!location) return null
+
+  return isLoaded && (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={{lng: 40, lat: 37}}
+      zoom={9}
+    >
+      {/* <MarkerClusterer>
+        < Marker
+          key={currentBusiness.id}
+          position={{ lat: 40, lng: 37 }}
+        />
+      </MarkerClusterer> */}
+    </GoogleMap>
+  )
+}
+
+export default Map;
